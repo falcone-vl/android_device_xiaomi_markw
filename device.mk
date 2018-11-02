8c7548dfede0a33b2bbda5cc2a4b8ede817e1f82
Razziell <razziell@ya.ru>
2018-11-02 17:15:07 +0300

    Add basic USB HAL that reports no status change

--- device.mk
+++ device.mk
@@ -353,7 +353,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi_markw
 
 # VNDK
 PRODUCT_PACKAGES += \
